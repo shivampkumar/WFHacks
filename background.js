45d6e9674d51c0c9e7bffecfa8acdf4cba86f929
Shivam Kumar <shikuma@microsoft.com>
2021-10-15 14:12:05 +0530

    Added permission function

---
+++ background.js
@@ -0,0 +1,18 @@
+navigator.mediaDevices.getUserMedia({audio: true})
+
+async function getUserMediaPermission (constraints) {
+    try {
+        const stream = await navigator.mediaDevices.getUserMedia (constraints)
+        if (stream) {
+            stream.tracks.forEach(track => track.stop())
+            return true
+        }
+        return false
+    } 
+    catch(error) {
+        return false
+    }
+}
+
+const permissionGranted = getUserMediaPermission ({audio:true})
+ if (!permissionGranted) console.log ('Hey! to use this you must grant permission!')
